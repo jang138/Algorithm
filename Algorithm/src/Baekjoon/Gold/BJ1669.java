5818c8ac3401231d22dfc0a53405c95d78a71791
jang138 <david9696@naver.com>
2022-12-13 00:00:29 +0900

    BJ1669

--- Algorithm/src/Baekjoon/Gold/BJ1669.java
+++ Algorithm/src/Baekjoon/Gold/BJ1669.java
@@ -1,14 +1,38 @@
 package Baekjoon.Gold;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  * https://www.acmicpc.net/problem/1669
  */
 
 public class BJ1669 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int diff = y - x;
+		
+		if (diff == 0) {
+		    System.out.println(0);
+		    return;
+		}
+		
+		double diff_root = Math.sqrt(diff);
+		long round = Math.round(diff_root);
 
+		
+		if( diff_root <= (double) round ) {
+			System.out.println(round*2 - 1);
+		} else {
+			System.out.println(round*2);
+		}
+		
 	}
 
 }
