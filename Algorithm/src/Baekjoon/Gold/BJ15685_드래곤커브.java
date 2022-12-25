25371779ff9d7b61b35bad2482642658589c22f7
jang138 <david9696@naver.com>
2022-12-26 04:40:21 +0900

    algo

--- Algorithm/src/Baekjoon/Gold/BJ15685_드래곤커브.java
+++ Algorithm/src/Baekjoon/Gold/BJ15685_드래곤커브.java
@@ -1,15 +1,33 @@
 package Baekjoon.Gold;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  * https://www.acmicpc.net/problem/15685
  * 골드4 드래곤 커브
  */
 
 public class BJ15685_드래곤커브 {
+	static boolean[][] visited = new boolean[101][101];
+	static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {1, 0, -1, 0};
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		int ans = 0;
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			int g = Integer.parseInt(st.nextToken());
+		}
+		
 	}
 
 }
