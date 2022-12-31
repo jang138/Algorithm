9c308f63bcbd056da62e4d1eab8e2bbfa7e139b8
jang138 <david9696@naver.com>
2022-12-31 23:48:22 +0900

    algo

---
+++ Algorithm/src/Baekjoon/Gold/BJ2293_동전1.java
@@ -0,0 +1,37 @@
+package Baekjoon.Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/problem/2293
+ * 골드5 동전1
+ */
+
+public class BJ2293_동전1 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[] dp = new int[k + 1];
+        dp[0] = 1;
+        
+		int[] coin = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = dp[j] + dp[j - coin[i]];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
