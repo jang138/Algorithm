ce9a72de32ef581952755d68ab68464f345b0a00
jang138 <david9696@naver.com>
2023-01-02 23:46:39 +0900

    algo

--- Algorithm/src/Baekjoon/Gold/BJ16234_인구이동.java
+++ Algorithm/src/Baekjoon/Gold/BJ16234_인구이동.java
@@ -1,5 +1,9 @@
 package Baekjoon.Gold;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  * https://www.acmicpc.net/problem/16234
  * 골드5 인구 이동
@@ -7,8 +11,22 @@ package Baekjoon.Gold;
 
 public class BJ16234_인구이동 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int N, L, R, day; // N: 땅크기 / 인구차이 L이상 R이하 / day: 
+	static int[][] map;
+	static int[][] visit;
+	static int[] dx = { 0, 0, 1, -1 };
+	static int[] dy = { 1, -1, 0, 0 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		
+		
 
 	}
 
