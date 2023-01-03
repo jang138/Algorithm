a9e699dd7e6797c5e7ac1881d3acee6c4c97cf91
jang138 <david9696@naver.com>
2023-01-03 12:01:27 +0900

    algo

--- Algorithm/src/Baekjoon/Gold/BJ16234_인구이동.java
+++ Algorithm/src/Baekjoon/Gold/BJ16234_인구이동.java
@@ -1,33 +1,114 @@
 package Baekjoon.Gold;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 /*
  * https://www.acmicpc.net/problem/16234
  * 골드5 인구 이동
+ * 참고 링크
+ * https://moonsbeen.tistory.com/259
  */
 
 public class BJ16234_인구이동 {
 
-	static int N, L, R, day; // N: 땅크기 / 인구차이 L이상 R이하 / day: 
+	static int n, l, r;
 	static int[][] map;
-	static int[][] visit;
-	static int[] dx = { 0, 0, 1, -1 };
-	static int[] dy = { 1, -1, 0, 0 };
+	static boolean[][] visit;
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { 1, 0, -1, 0 };
+	static ArrayList<Node> list; // 인구 이동이 필요한 노드 리스트
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		map = new int[N][N];
-		
-		
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
+		n = scan.nextInt();
+		l = scan.nextInt();
+		r = scan.nextInt();
+
+		map = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				map[i][j] = scan.nextInt();
+			}
+		}
+
+		System.out.println(move());
 	}
 
+	// 더 이상 인구이동이 일어나지 않을 때까지
+	public static int move() {
+		int result = 0;
+		
+		while (true) {
+			boolean isMove = false;
+			visit = new boolean[n][n];
+			
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (!visit[i][j]) {
+						int sum = bfs(i, j); // bfs탐색으로 열릴 수 있는 국경선 확인 하며 인구 이동할 총 인구수 반환
+						if (list.size() > 1) {
+							changePopulation(sum); // 열린 국경선 내의 노드들 인구 변경
+							isMove = true;
+						}
+					}
+				}
+			}
+			if (!isMove) {
+				return result;				
+			}
+			
+			result++;
+		}
+	}
+
+	public static int bfs(int x, int y) {
+		Queue<Node> q = new LinkedList<>();
+		list = new ArrayList<>();
+
+		q.offer(new Node(x, y));
+		list.add(new Node(x, y));
+		visit[x][y] = true;
+
+		int sum = map[x][y];
+		while (!q.isEmpty()) {
+			Node current = q.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int nx = current.x + dx[i];
+				int ny = current.y + dy[i];
+				if (nx >= 0 && ny >= 0 && nx < n && ny < n && !visit[nx][ny]) {
+					int diff = Math.abs(map[current.x][current.y] - map[nx][ny]);
+//					System.out.println(diff);
+					if (l <= diff && diff <= r) {
+						q.offer(new Node(nx, ny));
+						list.add(new Node(nx, ny));
+						sum += map[nx][ny];
+						visit[nx][ny] = true;
+					}
+				}
+			}
+		}
+		return sum;
+	}
+
+	public static void changePopulation(int sum) {
+		int avg = sum / list.size();
+		for (Node n : list) {
+			map[n.x][n.y] = avg;
+		}
+	}
+
+	public static class Node {
+		int x;
+		int y;
+
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
--- Algorithm/src/Baekjoon/Gold/BJ2225_합분해.java
+++ Algorithm/src/Baekjoon/Gold/BJ2225_합분해.java
@@ -9,31 +9,30 @@ import java.util.StringTokenizer;
  * 골드5 합분해
  */
 
-
 public class BJ2225_합분해 {
 
 	static long dp[][] = new long[201][201];
-	 
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String tc = br.readLine();
 		StringTokenizer st = new StringTokenizer(tc, " ");
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		for(int i=0; i<201; i++){
+		for (int i = 0; i < 201; i++) {
 			dp[i][0] = 1;
 			dp[1][i] = 1;
 		}
-		for(int i=1; i<201; i++){
-			dp[2][i] = i+1;
+		for (int i = 1; i < 201; i++) {
+			dp[2][i] = i + 1;
 		}
-		for(int i=3; i<201; i++){
-			for(int j=1; j<201; j++){
-				for(int k1=0; k1<=j; k1++){
-					dp[i][j]+= dp[i-1][j-k1]%1000000000;
+		for (int i = 3; i < 201; i++) {
+			for (int j = 1; j < 201; j++) {
+				for (int k1 = 0; k1 <= j; k1++) {
+					dp[i][j] += dp[i - 1][j - k1] % 1000000000;
 				}
 			}
 		}
-		System.out.println(dp[k][n]%1000000000);
+		System.out.println(dp[k][n] % 1000000000);
 	}
 }
