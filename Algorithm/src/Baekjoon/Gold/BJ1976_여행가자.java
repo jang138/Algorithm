0acb2353b5a485b52c9b8a1bb2acf2fa4b9f394a
jang138 <david9696@naver.com>
2023-01-07 23:55:25 +0900

    algo

---
+++ Algorithm/src/Baekjoon/Gold/BJ1976_여행가자.java
@@ -0,0 +1,15 @@
+package Baekjoon.Gold;
+
+/*
+ * https://www.acmicpc.net/problem/1976
+ * 골드4 여행 가자
+ */
+
+public class BJ1976_여행가자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
