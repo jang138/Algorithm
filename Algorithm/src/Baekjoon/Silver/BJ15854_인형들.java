235a21275602b96f78cd4f1c8e5273bd630cceee
jang138 <david9696@naver.com>
2023-01-09 22:35:24 +0900

    algo

---
+++ Algorithm/src/Baekjoon/Silver/BJ15854_인형들.java
@@ -0,0 +1,15 @@
+package Baekjoon.Silver;
+
+/*
+ * https://www.acmicpc.net/problem/15954
+ * 실버1 인형들
+ */
+
+public class BJ15854_인형들 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
