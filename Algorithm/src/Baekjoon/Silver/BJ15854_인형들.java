8d33d840ab8b43e6a33013141db37dbe983648e1
jang138 <david9696@naver.com>
2023-01-10 22:53:11 +0900

    algo

--- Algorithm/src/Baekjoon/Silver/BJ15854_인형들.java
+++ Algorithm/src/Baekjoon/Silver/BJ15854_인형들.java
@@ -1,5 +1,9 @@
 package Baekjoon.Silver;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  * https://www.acmicpc.net/problem/15954
  * 실버1 인형들
@@ -7,9 +11,13 @@ package Baekjoon.Silver;
 
 public class BJ15854_인형들 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        double A[] = new double[N];
 	}
 
 }
