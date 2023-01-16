c24be0f6841d887a3277ceb2ddbf6dc28722a17b
jang138 <david9696@naver.com>
2023-01-16 14:15:46 +0900

    algo

--- Algorithm/src/Baekjoon/Gold/BJ1339_단어수학.java
+++ Algorithm/src/Baekjoon/Gold/BJ1339_단어수학.java
@@ -3,7 +3,12 @@ package Baekjoon.Gold;
 public class BJ1339_단어수학 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("hi ");
+		sb.append("dear").append("!!");
+		System.out.println(sb);
+		System.out.println(sb.toString());
 
 	}
 
