39513b653ecf4fb3148456a977494008f4585a55
jang138 <david9696@naver.com>
2023-01-31 21:17:53 +0900

    algo

--- Algorithm/src/algoExercise/Day230131_QueueAPI.java
+++ Algorithm/src/algoExercise/Day230131_QueueAPI.java
@@ -1,10 +1,28 @@
 package algoExercise;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Day230131_QueueAPI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Queue<String> queue = new LinkedList<String>();
+		
+		System.out.println(queue.toString());
+		queue.offer("김");
+		queue.offer("이");
+		queue.offer("박");
+		
+		System.out.println(queue.toString());
+		System.out.println(queue.poll());
+		System.out.println(queue.toString());
+		System.out.println(queue.poll());
+		System.out.println(queue.toString());
+		System.out.println(queue.peek());
+		System.out.println(queue.toString());
+		System.out.println(queue.poll());
+		System.out.println(queue.toString());
 	}
 
 }
