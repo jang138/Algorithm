3b6d6affc37893ce6b3e0466ea75fa21349bb325
jang138 <david9696@naver.com>
2023-02-06 23:58:56 +0900

    algo

---
+++ Algorithm/src/Baekjoon/gold/BJ2036_수열의점수.java
@@ -0,0 +1,15 @@
+package Baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ2036_수열의점수 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+
+	}
+
+}
