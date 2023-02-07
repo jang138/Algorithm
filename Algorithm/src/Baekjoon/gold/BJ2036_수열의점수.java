10786b97f0ebd5a8508e18c9f9637482fcf11e46
jang138 <david9696@naver.com>
2023-02-07 23:24:19 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ2036_수열의점수.java
+++ Algorithm/src/Baekjoon/gold/BJ2036_수열의점수.java
@@ -15,60 +15,65 @@ public class BJ2036_수열의점수 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		long ans = 0;
 
 		// 수열을 양수, 음수, 1로 나눈다 ==> 양수X양수, 음수X음수로 최대값을 만들기 위함, 1은 더하는게 이득
-		ArrayList<Long> plus = new ArrayList<Long>();
-		ArrayList<Long> minus = new ArrayList<Long>();
+		ArrayList<Long> posNum = new ArrayList<Long>();
+		ArrayList<Long> negNum = new ArrayList<Long>();
 		ArrayList<Long> one = new ArrayList<Long>();
 
 		for (int i = 0; i < n; i++) {
 			long num = Integer.parseInt(br.readLine());
 			if (num > 1)
-				plus.add(num);
+				posNum.add(num);
 			else if (num <= 0)
-				minus.add(num);
+				negNum.add(num);
 			else if (num == 1)
 				one.add(num);
 		}
 
 		// 절대값이 큰 순서로 정렬 (양수는 내림차순, 음수는 오름차순)
-		Collections.sort(plus, Collections.reverseOrder());
-		Collections.sort(minus);
+		Collections.sort(posNum, Collections.reverseOrder());
+		Collections.sort(negNum);
 
+		// 양수계산
+		long posNumSum = 0;
+		
 		// 짝수인 경수
-		if (plus.size() % 2 == 0) {
-			for (int i = 0; i < plus.size(); i += 2) {
-				ans = ans + (plus.get(i) * plus.get(i + 1));
+		if (posNum.size() % 2 == 0) {
+			for (int i = 0; i < posNum.size(); i += 2) {
+				posNumSum = posNumSum + (posNum.get(i) * posNum.get(i + 1));
 			}
 		}
 
 		// 음수인 경우
-		else if (plus.size() % 2 != 0) {
-			for (int i = 0; i < plus.size() - 1; i += 2) {
-				ans = ans + (plus.get(i) * plus.get(i + 1));
+		else if (posNum.size() % 2 != 0) {
+			for (int i = 0; i < posNum.size() - 1; i += 2) {
+				posNumSum = posNumSum + (posNum.get(i) * posNum.get(i + 1));
 			}
 			// 마지막 남는 것 계산
-			ans = ans + plus.get(plus.size() - 1);
+			posNumSum = posNumSum + posNum.get(posNum.size() - 1);
 		}
-
+		
+		// 음수계산
+		long negNumSum = 0;
+		
 		// 짝수인 경우
-		if (minus.size() % 2 == 0)
-			for (int i = 0; i < minus.size(); i += 2) {
-				ans = ans + (minus.get(i) * minus.get(i + 1));
+		if (negNum.size() % 2 == 0)
+			for (int i = 0; i < negNum.size(); i += 2) {
+				negNumSum = negNumSum + (negNum.get(i) * negNum.get(i + 1));
 			}
 
 		// 음수인 경우
-		else if (minus.size() % 2 != 0) {
-			for (int i = 0; i < minus.size() - 1; i += 2) {
-				ans = ans + (minus.get(i) * minus.get(i + 1));
+		else if (negNum.size() % 2 != 0) {
+			for (int i = 0; i < negNum.size() - 1; i += 2) {
+				negNumSum = negNumSum + (negNum.get(i) * negNum.get(i + 1));
 			}
 			// 마지막 남는 것 계산
-			ans = ans + minus.get(minus.size() - 1);
+			negNumSum = negNumSum + negNum.get(negNum.size() - 1);
 		}
 
 		// 남은 1들을 더해준다
-		ans = ans + one.size();
+		long ans = posNumSum+ negNumSum + one.size();
 		System.out.println(ans);
 	}
 }
