86559dfacd3cf11adbef6995981540ea035c4f2b
jang138 <david9696@naver.com>
2023-02-07 15:04:34 +0900

    algo

---
+++ Algorithm/src/algoExercise/Tree/CompleteBinaryTreeTest.java
@@ -0,0 +1,18 @@
+package algoExercise.Tree;
+
+public class CompleteBinaryTreeTest {
+
+	public static void main(String[] args) {
+
+		int size = 9;
+		CompleteBinaryTree tree = new CompleteBinaryTree(size);
+
+		for (int i = 0; i < size; i++) {
+			tree.add((char) (65 + i)); // A, B, C, D ...
+		}
+		
+		tree.bfs();
+		tree.bfs2();
+	}
+
+}
