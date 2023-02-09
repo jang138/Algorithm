41750cce9ed5e1053d1eaf768c5fac203c83e675
jang138 <david9696@naver.com>
2023-02-09 22:23:49 +0900

    algo

---
+++ Algorithm/src/Baekjoon/gold/BJ17352_여러분의다리가되어드리겠습니다.java
@@ -0,0 +1,15 @@
+package Baekjoon.gold;
+
+/*
+ * https://www.acmicpc.net/problem/17352
+ * 골드5 여러분의 다리가 되어 드리겠습니다!
+ */
+
+public class BJ17352_여러분의다리가되어드리겠습니다 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
