d41991691f21eb5dbd07f0a9de9a76e4213c13dc
jang138 <david9696@naver.com>
2023-02-11 20:54:21 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ1253_좋다.java
+++ Algorithm/src/Baekjoon/gold/BJ1253_좋다.java
@@ -2,6 +2,7 @@ package Baekjoon.gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
@@ -15,13 +16,44 @@ public class BJ1253_좋다 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int arr[] = new int[N];
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		
+
+		int ans = 0;
+		Arrays.sort(arr);
+
+		for (int i = 0; i < N; i++) {
+			int left = 0;
+			int right = N - 1;
+			int target = arr[i];
+
+			while (true) {
+				// 현재 숫자와 같으면 안되니까 바로 통과
+				if (left == i)
+					left++;
+				else if (right == i)
+					right--;
+
+				if (left >= right) // 탐색 실패
+					break;
+
+				int sum = arr[left] + arr[right];
+
+				if (sum > target)
+					right--;
+				else if (sum < target)
+					left++;
+				else {
+					ans++;
+					break;
+				}
+			}
+		}
+
+		System.out.println(ans);
 	}
 
 }
