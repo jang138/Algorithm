007bd9977f360c2201f5aa6579f2a4208e54e895
jang138 <david9696@naver.com>
2023-02-12 23:33:22 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ17352_여러분의다리가되어드리겠습니다.java
+++ Algorithm/src/Baekjoon/gold/BJ17352_여러분의다리가되어드리겠습니다.java
@@ -1,5 +1,8 @@
 package Baekjoon.gold;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /*
  * https://www.acmicpc.net/problem/17352
  * 골드5 여러분의 다리가 되어 드리겠습니다!
@@ -8,7 +11,9 @@ package Baekjoon.gold;
 public class BJ17352_여러분의다리가되어드리겠습니다 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
 
 	}
 
