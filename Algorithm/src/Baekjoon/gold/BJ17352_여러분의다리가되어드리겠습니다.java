f468577150fed545e19a0acb2cb4055be1e49022
jang138 <david9696@naver.com>
2023-02-14 00:39:05 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ17352_여러분의다리가되어드리겠습니다.java
+++ Algorithm/src/Baekjoon/gold/BJ17352_여러분의다리가되어드리겠습니다.java
@@ -2,6 +2,7 @@ package Baekjoon.gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /*
  * https://www.acmicpc.net/problem/17352
@@ -10,11 +11,51 @@ import java.io.InputStreamReader;
 
 public class BJ17352_여러분의다리가되어드리겠습니다 {
 
+	static int[] parent;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		
-		parent[] = 
+		parent = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+
+		for (int i = 0; i < N - 2; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			union(a, b);
+		}
+
+		for (int i = 2; i <= N; i++) {
+			if (!isSameParent(1, i)) {
+				System.out.print(1 + " " + i);
+				return;
+			}
+		}
+
+	}
+
+	static int find(int a) {
+		if (parent[a] == a)
+			return a;
+		else
+			return parent[a] = find(parent[a]);
+	}
+
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+
+		if (a < b)
+			parent[b] = a;
+		else
+			parent[a] = b;
+	}
+
+	static boolean isSameParent(int a, int b) {
+		return find(a) == find(b);
 	}
 
 }
