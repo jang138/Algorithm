d91b6f2d182c8c69132fc59723e98b80adbf1090
jang138 <david9696@naver.com>
2023-02-14 17:29:37 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ19539_사과나무.java
+++ Algorithm/src/Baekjoon/gold/BJ19539_사과나무.java
@@ -1,10 +1,30 @@
 package Baekjoon.gold;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BJ19539_사과나무 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int sum = 0; // 전체 합
+		int cnt = 0; // 2를 사용할 수 있는 횟수 합
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			int atwo = a / 2;
+			sum += a;
+			cnt += atwo;
+		}
+
+		if (sum % 3 == 0 && cnt >= sum / 3) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
 	}
 
 }
