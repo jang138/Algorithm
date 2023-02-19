fa9b3f66a014d535cd26ffed137341bb236717c9
jang138 <david9696@naver.com>
2023-02-20 01:07:49 +0900

    algo

--- Algorithm/src/Softeer/test.java
+++ Algorithm/src/Softeer/test.java
@@ -1,19 +1,12 @@
 package Softeer;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class test {
 	public static void main(String args[]) throws Exception {
 
-
-		String st = "09";
-		
-		int a = Integer.parseInt(st);
 		
 		
 		
-
-		System.out.println(a);
 	}
 }
