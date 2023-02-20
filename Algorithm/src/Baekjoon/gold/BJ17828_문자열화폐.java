708199e18d331dbde2701217b754d29280775785
jang138 <david9696@naver.com>
2023-02-20 19:54:48 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ17828_문자열화폐.java
+++ Algorithm/src/Baekjoon/gold/BJ17828_문자열화폐.java
@@ -1,10 +1,45 @@
 package Baekjoon.gold;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/problem/17828
+ * 골드5 문자열 화폐
+ */
+
 public class BJ17828_문자열화폐 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
 
+		if (N * 26 < X || X < N) {
+			System.out.println("!");
+			return;
+		}
+
+		char[] str = new char[N];
+
+		for (int i = 0; i < N; i++) {
+			str[i] = 'A';
+		}
+
+		X -= N;
+
+		for (int i = N - 1; i >= 0 && X > 0; i--) {
+			int ch = Math.min(X, 25);
+			str[i] += (char) ch;
+			X -= ch;
+		}
+		StringBuilder sb = new StringBuilder(N);
+		for (int i = 0; i < N; i++) {
+			sb.append(str[i]);
+		}
+		System.out.println(sb);
 	}
 
 }
--- Algorithm/src/Softeer/test.java
+++ Algorithm/src/Softeer/test.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class test {
 	public static void main(String args[]) throws Exception {
 
-		
+		Scanner sc = new Scanner(System.in);
+
+		char ch = sc.nextLine().charAt(0);
+		int num = (int) ch - 64;
+		System.out.println(num);
 		
 		
 	}
