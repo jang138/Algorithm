eac18b7b49897ecbceef3865ca01af9d2027f42a
jang138 <david9696@naver.com>
2023-02-20 19:04:50 +0900

    algo

---
+++ Algorithm/src/Baekjoon/gold/BJ17828_문자열화폐.java
@@ -0,0 +1,10 @@
+package Baekjoon.gold;
+
+public class BJ17828_문자열화폐 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Algorithm/src/Baekjoon/gold/BJ2151_거울설치.java
+++ Algorithm/src/Baekjoon/gold/BJ2151_거울설치.java
@@ -17,15 +17,13 @@ import java.util.PriorityQueue;
 *.!.*
 *#***
 
+2
+!#
+!#
+
 */
 
 public class BJ2151_거울설치 {
-
-	static int N, mirror, ans;
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
-	static char[][] map;
-	static boolean[][][] visit; // 방문할 때 빛이 들어오는 방향까지 3차원 배열
 
 	// 빛의 진행
 	static class Node {
@@ -38,16 +36,13 @@ public class BJ2151_거울설치 {
 			this.dir = dir;
 		}
 
-		@Override
-		public String toString() {
-			return "Node [x=" + x + ", y=" + y + ", mirror=" + mirror + ", dir=" + dir + "]";
-		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new char[N][N];
+		int N = Integer.parseInt(br.readLine());
+		char[][] map = new char[N][N];
+		boolean visit[][][] = new boolean[4][N][N]; // 방문할 때 빛이 들어오는 방향까지 3차원 배열
 		Node door = null;
 
 		for (int i = 0; i < N; i++) {
@@ -62,7 +57,7 @@ public class BJ2151_거울설치 {
 		}
 
 		// 거울의 수 오름차순
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
+		PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> {
 			return o1.mirror - o2.mirror;
 		});
 
@@ -73,13 +68,15 @@ public class BJ2151_거울설치 {
 			visit[i][door.x][door.y] = true;
 		}
 
-		// 빛이 문에서 다른 문으로 진행하면서 만난 거울(!)의 수가 최소가 되는 것을 출력한다.
-		flag: while (!pq.isEmpty()) {
-			Node current = pq.poll();
-			System.out.println(current);
+		int[] dx = { -1, 0, 1, 0 };
+		int[] dy = { 0, 1, 0, -1 };
+		int[] dir3 = { -1, 1, 0 }; // 좌회전 -1, 우회전 1, 직진 0
 
-			// 직진 0, 좌회전 -1, 우회전 1
-			for (int i = -1; i <= 1; i++) {
+		// 빛이 문에서 다른 문으로 진행하면서 만난 거울(!)의 수가 최소가 되는 것을 출력한다.
+		while (!pq.isEmpty()) {
+			Node current = pq.poll();
+
+			for (int i : dir3) {
 				// 거울을 놓을수 있는 자리가 아니면 직진 밖에 하지 않는다
 				if (map[current.x][current.y] != '!' && i != 0)
 					continue;
@@ -87,13 +84,8 @@ public class BJ2151_거울설치 {
 				int dir = (current.dir + i + 4) % 4;
 				int nx = current.x + dx[dir];
 				int ny = current.y + dy[dir];
-
 				// i==0 직진해서 거울이 필요 없는경우, i!=0 빛이 꺾이면서 거울이 필요한 경우
-				if (i == 0) {
-					mirror = current.mirror;
-				} else {
-					mirror = current.mirror + 1;
-				}
+				int mirror = (i == 0 ? current.mirror : current.mirror + 1);
 
 				if (nx < 0 || ny < 0 || nx >= N || ny >= N || map[nx][ny] == '*')
 					continue;
@@ -103,8 +95,8 @@ public class BJ2151_거울설치 {
 
 				// 반대편 문에 도달, while 종료
 				if (map[nx][ny] == '#') {
-					ans = mirror;
-					break flag;
+					System.out.println(mirror);
+					return;
 				}
 
 				// 새로운 빛의 진행 추가
@@ -113,8 +105,6 @@ public class BJ2151_거울설치 {
 
 			}
 		}
-
-		System.out.println(ans);
 	}
 
 }
