38f1cd2020e3ba9226dbc58ce13b63248c0b2452
jang138 <david9696@naver.com>
2023-02-27 17:10:42 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ16400_소수화폐.java
+++ Algorithm/src/Baekjoon/gold/BJ16400_소수화폐.java
@@ -1,5 +1,6 @@
 package Baekjoon.gold;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,12 +11,17 @@ import java.util.Scanner;
 
 public class BJ16400_소수화폐 {
 
+	static final int MOD = 123456789;
 	static boolean prime[];
+	static ArrayList<Integer> primeList;
+	static int dp[];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		prime = new boolean[N + 1];
+		primeList = new ArrayList<>();
+		dp = new int[N + 1];
 
 		Arrays.fill(prime, true);
 		// 소수는 true 아니면 false
@@ -27,13 +33,23 @@ public class BJ16400_소수화폐 {
 				prime[j] = false;
 			}
 		}
-		
+
 		for (int i = 0; i <= N; i++) {
-			if(prime[i])
-				System.out.println(i);
+			if (prime[i]) {
+//				System.out.println(i);
+				primeList.add(i);
+			}
 		}
-		
-		
+
+		dp[0] = 1;
+		for (int i = 0; i < primeList.size(); i++) {
+			int primeMoney = primeList.get(i);
+			for (int j = primeMoney; j <= N; j++) {
+				dp[j] = (dp[j] + dp[j - primeMoney]) % MOD;
+			}
+		}
+
+		System.out.println(dp[N]);
 
 	}
 
