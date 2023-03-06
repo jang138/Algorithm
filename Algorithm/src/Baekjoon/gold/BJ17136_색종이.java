dc61b44aded621c9c7f0ad922893477c323b65f0
jang138 <david9696@naver.com>
2023-03-07 01:30:21 +0900

    algo

---
+++ Algorithm/src/Baekjoon/gold/BJ17136_색종이.java
@@ -0,0 +1,15 @@
+package Baekjoon.gold;
+
+/*
+ * https://www.acmicpc.net/problem/17136
+ * 골드2 색종이 붙이기
+ */
+
+public class BJ17136_색종이 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
