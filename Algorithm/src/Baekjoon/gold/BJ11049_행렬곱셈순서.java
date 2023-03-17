65d64a7467776779881ce45471b9628d5b8abefd
jang138 <david9696@naver.com>
2023-03-17 15:39:11 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ11049_행렬곱셈순서.java
+++ Algorithm/src/Baekjoon/gold/BJ11049_행렬곱셈순서.java
@@ -7,12 +7,13 @@ import java.util.StringTokenizer;
 public class BJ11049_행렬곱셈순서 {
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(reader.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int[][] a = new int[N][2];
 		int[][] dp = new int[N][N];
+		
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(reader.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			a[i][0] = Integer.parseInt(st.nextToken());
 			a[i][1] = Integer.parseInt(st.nextToken());
 		}
@@ -21,8 +22,8 @@ public class BJ11049_행렬곱셈순서 {
 			for (int j = 0; j < N - i; j++) {
 				dp[j][j + i] = Integer.MAX_VALUE;
 				for (int k = 0; k < i; k++) {
-					int cost = dp[j][j + k] + dp[j + k + 1][j + i] + a[j][0] * a[j + k][1] * a[j + i][1];
-					dp[j][j + i] = Math.min(dp[j][j + i], cost);
+					int target = dp[j][j + k] + dp[j + k + 1][j + i] + a[j][0] * a[j + k][1] * a[j + i][1];
+					dp[j][j + i] = Math.min(dp[j][j + i], target);
 				}
 			}
 		}
---
+++ Algorithm/src/Baekjoon/gold/BJ1188_음식평론가.java
@@ -0,0 +1,15 @@
+package Baekjoon.gold;
+
+/*
+ * https://www.acmicpc.net/problem/1188
+ * 골드4 음식 평론가
+ */
+
+public class BJ1188_음식평론가 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
