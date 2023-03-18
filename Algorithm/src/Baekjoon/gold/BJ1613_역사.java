a27d4134fbf75a9fb5eaa7e256fdb858967f8388
jang138 <david9696@naver.com>
2023-03-19 00:00:28 +0900

    algo

--- Algorithm/src/Baekjoon/gold/BJ1613_역사.java
+++ Algorithm/src/Baekjoon/gold/BJ1613_역사.java
@@ -1,5 +1,9 @@
 package Baekjoon.gold;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  * https://www.acmicpc.net/problem/1613
  * 골드3 역사
@@ -7,9 +11,14 @@ package Baekjoon.gold;
 
 public class BJ1613_역사 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		
 	}
 
 }
