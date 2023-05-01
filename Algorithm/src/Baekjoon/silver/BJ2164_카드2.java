63b45a7232b6198964a6b0628bf48958a342cc35
jang138 <david9696@naver.com>
2023-05-01 18:17:52 +0900

    algo

--- Algorithm/src/Baekjoon/silver/BJ2164_카드2.java
+++ Algorithm/src/Baekjoon/silver/BJ2164_카드2.java
@@ -14,21 +14,21 @@ public class BJ2164_카드2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		
+
 		Queue<Integer> queue = new LinkedList<>();
-		for(int i = 1; i <=N; i++) {
+		for (int i = 1; i <= N; i++) {
 			queue.add(i);
 		}
-		
-		while(queue.size() > 1) {
+
+		while (queue.size() > 1) {
 			// 1. 맨 위 카드를 버린다.
 			queue.poll();
-			
+
 			// 2. 다음 맨 위 카드는 맨 아래로 넣는다.
 			int bottom = queue.poll();
 			queue.add(bottom);
 		}
-		
+
 		System.out.println(queue.poll());
 	}
 
