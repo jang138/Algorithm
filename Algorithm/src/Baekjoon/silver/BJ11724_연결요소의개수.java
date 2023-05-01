058c3745fee63853489f442e912a50812c3b91f2
jang138 <david9696@naver.com>
2023-05-01 21:01:56 +0900

    algo

---
+++ Algorithm/src/Baekjoon/silver/BJ11724_연결요소의개수.java
@@ -0,0 +1,83 @@
+package Baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/problem/11724
+ * 실버2 연결 요소의 개수
+ */
+
+public class BJ11724_연결요소의개수 {
+
+	static ArrayList<Integer>[] A;
+	static boolean visit[];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		A = new ArrayList[N + 1];
+		visit = new boolean[N + 1];
+
+		for (int i = 0; i <= N; i++) {
+			A[i] = new ArrayList<Integer>();
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+
+			// 양방향 에지
+			A[s].add(e);
+			A[e].add(s);
+		}
+
+//		for (int i = 1; i <= N; i++) {
+//			System.out.print(i + " => ");
+//			for (int j = 0; j < A[i].size(); j++) {
+//				System.out.print(A[i].get(j) + " ");
+//			}
+//			System.out.println();
+//		}
+
+		for (int i = 1; i <= N; i++) {
+			System.out.print(i + " => ");
+			for (int j : A[i]) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+		int cnt = 0;
+
+		for (int i = 1; i <= N; i++) {
+			if (!visit[i]) {
+				cnt++;
+				dfs(i);
+			}
+		}
+
+		System.out.println(cnt);
+	}
+
+	private static void dfs(int v) {
+		if (visit[v]) {
+			return;
+		}
+
+		visit[v] = true;
+
+		for (int i : A[v]) {
+			if (visit[i] == false) {
+				dfs(i);
+			}
+		}
+	}
+
+}
--- Algorithm/src/Softeer/test.java
+++ Algorithm/src/Softeer/test.java
@@ -1,36 +1,24 @@
 package Softeer;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class test {
 
 	public static void main(String[] args) throws Exception {
-        
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-        
-        Stack<Integer> stack = new Stack<>();  // 인덱스를 저장할 스택
-        int[] result = new int[n];  // 오큰수를 저장할 배열. 0으로 초기화.
-        
-        for (int i = 0; i < n; i++) {
-            // 스택이 비어있지 않으면서, 스택의 마지막 값이 현재 원소보다 작은 경우
-            while (!stack.empty() && a[stack.peek()] < a[i]) {
-                result[stack.pop()] = a[i];  // 스택의 마지막 값의 오큰수를 현재 원소로 갱신
-            }
-            stack.push(i);  // 현재 원소 인덱스를 스택에 추가
-        }
-        
-        while (!stack.empty()) {
-            result[stack.pop()] = -1;  // 오큰수가 없는 경우 -1로 설정
-        }
-        
-        for (int i = 0; i < n; i++) {
-            System.out.print(result[i] + " ");
-        }
-    }
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int input[] = new int[N];
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(input);
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(input[i]);
+		}
+	}
 }
