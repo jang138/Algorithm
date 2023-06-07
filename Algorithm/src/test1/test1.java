0ede4087da67a23a3750311e57bbfe656ceb9ea4
jang138 <david9696@naver.com>
2023-06-07 16:05:30 +0900

    algo

--- Algorithm/src/test1/test1.java
+++ Algorithm/src/test1/test1.java
@@ -1,22 +1,68 @@
 package test1;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class test1 {
 
+	static int dx[] = { 1, 0, -1, 0 };
+	static int dy[] = { 0, 1, 0, -1 };
+	static int map[][];
+	static boolean visit[][];
+	static int N, M;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		char C[][] = new char[N][M];
-		
 
-		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		visit = new boolean[N][M];
+
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+
+			for (int j = 0; j < M; j++) {
+				map[i][j] = line.charAt(j) - '0';
+				// System.out.print(map[i][j]);
+			}
+			// System.out.println();
+		}
+
+		bfs(0, 0);
+		System.out.println(map[N - 1][M - 1]);
+	}
+
+	static void bfs(int i, int j) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] { i, j });
+
+		while (!queue.isEmpty()) {
+			int now[] = queue.poll();
+			visit[i][j] = true;
+
+			for (int d = 0; d < 4; d++) {
+				int x = now[0] + dx[d];
+				int y = now[1] + dy[d];
+
+				if (x < 0 || y < 0 || x >= N || y >= M) {
+					continue;
+				}
+
+				if (visit[x][y] || map[x][y] == 0) {
+					continue;
+				}
+
+				queue.add(new int[] { x, y });
+				map[x][y] = map[now[0]][now[1]] + 1;
+				visit[x][y] = true;
+			}
+		}
+
 	}
 
 }
