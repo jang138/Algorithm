473ef3e099e432038465358d68735be791143d81
jang138 <david9696@naver.com>
2023-06-28 17:43:40 +0900

    algo

--- Algorithm/src/Baekjoon/silver/BJ10844_쉬운계단수.java
+++ Algorithm/src/Baekjoon/silver/BJ10844_쉬운계단수.java
@@ -9,18 +9,48 @@ import java.util.Scanner;
 
 public class BJ10844_쉬운계단수 {
 
+	final static long mod = 1000000000;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		long dp[] = new long[N + 1];
-		dp[1] = 9;
-		dp[2] = 17;
-		
-		for (int i = 3; i <= N; i++) {
-			dp[i] = (2 * (dp[i - 1] - 2) + 2) % 1000000000;
+
+		/*
+		 * dp[i][j] = i번째 자리수의 값이 j일때 
+		 * dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1] 
+		 * 단 끝자리가 0이나 9인 경우, 1과 8 밖에 오지 않는다.
+		 */
+
+		long dp[][] = new long[N + 1][10];
+
+		// 첫번째 자리수 = 일의 자리이므로 경우의 수가 하나씩 밖에 없다.
+		for (int i = 1; i < 10; i++) {
+			dp[1][i] = 1;
 		}
 
-		System.out.println(dp[N]);
+		for (int i = 2; i <= N; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (j == 0) {
+					dp[i][0] = dp[i - 1][1] % mod;
+					continue;
+				}
+
+				if (j == 9) {
+					dp[i][9] = dp[i - 1][8] % mod;
+					continue;
+				}
+
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+			}
+		}
+
+		long sum = 0;
+		
+		for (int i = 0; i < 10; i++) {
+			sum += dp[N][i];
+		}
+		
+		System.out.println(sum % mod);
 	}
 
 }
