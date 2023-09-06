c64f845a5a3bfec0461bdd23ac34ffe8cf253a8a
jang <david9696@naver.com>
2023-09-06 18:41:10 +0900

    algo

---
+++ Algorithm/src/Baekjoon/silver/BJ11725_트리의부모찾기_dfs.java
@@ -0,0 +1,64 @@
+package Baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/problem/11725
+ * 실버2 트리의 부모 찾기
+ */
+
+public class BJ11725_트리의부모찾기_dfs {
+
+	static int N;
+	static boolean[] visit;
+	static int[] parentNode;
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(br.readLine());
+		visit = new boolean[N + 1];
+		parentNode = new int[N + 1];
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i < N - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			graph.get(x).add(y);
+			graph.get(y).add(x);
+		}
+
+		dfs(1);
+
+		for (int i = 2; i <= N; i++) {
+			sb.append(parentNode[i]).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	private static void dfs(int n) {
+		visit[n] = true;
+
+		for (int node : graph.get(n)) {
+			if (!visit[node]) {
+				visit[node] = true;
+				parentNode[node] = n;
+				dfs(node);
+			}
+		}
+	}
+
+}
---
+++ Algorithm/src/Baekjoon/silver/BJ1991_트리순회.java
@@ -0,0 +1,104 @@
+package Baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/problem/1991
+ * 실버1 트리 순회
+ */
+
+public class BJ1991_트리순회 {
+
+	static int N;
+	static Node head = new Node('A', null, null);
+
+	static class Node {
+		char n;
+		Node left;
+		Node right;
+
+		public Node(char n, Node left, Node right) {
+			this.n = n;
+			this.left = left;
+			this.right = right;
+		}
+
+		public String toString() {
+			return "Node [n=" + n + ", left=" + left + ", right=" + right + "]";
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			char n = st.nextToken().charAt(0);
+			char l = st.nextToken().charAt(0);
+			char r = st.nextToken().charAt(0);
+
+			makeTree(head, n, l, r, 1);
+		}
+
+		preOrder(head);
+		System.out.println();
+
+		inOrder(head);
+		System.out.println();
+
+		postOrder(head);
+		System.out.println();
+
+	}
+
+	private static void makeTree(Node temp, char root, char left, char right, int depth) {
+
+		if (temp.n == root) {
+			temp.left = left == '.' ? null : new Node(left, null, null);
+			temp.right = right == '.' ? null : new Node(right, null, null);
+		} else {
+			if (temp.left != null) {
+				makeTree(temp.left, root, left, right, depth + 1);
+
+			}
+			if (temp.right != null) {
+				makeTree(temp.right, root, left, right, depth + 1);
+
+			}
+		}
+
+	}
+
+	private static void preOrder(Node node) {
+		if (node == null)
+			return;
+
+		System.out.print(node.n);
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+
+	private static void inOrder(Node node) {
+		if (node == null)
+			return;
+
+		inOrder(node.left);
+		System.out.print(node.n);
+		inOrder(node.right);
+	}
+
+	private static void postOrder(Node node) {
+		if (node == null)
+			return;
+
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print(node.n);
+	}
+}
